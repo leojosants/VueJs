4ab95647b6a327096ac3d04998d02dc3cbf511b4
Leonardo Santos <leonardojosantos@gmail.com>
2022-08-09 17:20:10 -0300

    Renderização de listas (v-for)
    Neste vídeo você vai aprender a renderizar listas, ou seja, imprimir arrays no HTML com #Vue, vamos utilizar uma diretiva bem famosa: a v-for, que faz um loop em um array, que pode também ser um array de objetos.

--- projeto_vue/src/components/Info.vue
+++ projeto_vue/src/components/Info.vue
@@ -3,16 +3,26 @@
     <div>
         <p v-if="esta_trabalhando">Estou trabalhando no momento.</p>
         <p v-else>Estou em busca de novas oportunidades!</p>
-        <p>Utilizo as seguinte tecnologias:</p>
+        <p>Utilizo as seguinte tecnologias para back-end:</p>
         
         <ul>
-            <li>JavaScript</li>
-            <li>PHP</li>
-            <li>Python</li>
+            <li v-for="(technology, index) in backend_technologies" v-bind:key="index">
+                {{ technology }}
+            </li>
+        </ul>
+
+        <p>Utilizo as seguinte tecnologias para front-end:</p>
+
+        <ul>
+            <li v-for="technology in frontend_technologies" :key="technology.id">
+                {{ technology.language }}
+            </li>
         </ul>
 
         <div>
-            <button @click="showEmail">{{ textoBotao }}</button>
+            <button @click="showEmail">
+                {{ textoBotao }}
+            </button>
         </div>
 
         <p v-show="mostrar_email">Mande uma memsagem para: {{ email }}</p>
@@ -37,7 +47,17 @@
             mostrar_email: false,
             email: "leo@gmail",
             meu_link: "https://github.com/leojosants",
-            textoBotao: 'Mostrar e-mail'
+            textoBotao: 'Mostrar e-mail',
+            
+            backend_technologies: [
+                'JavaScript', 'PHP', 'Python'
+            ],
+            
+            frontend_technologies: [
+                {id: 1, language: 'HTML'},
+                {id: 2, language: 'CSS'},
+                {id: 3, language: 'Vue'}
+            ]
         };
     },
 
