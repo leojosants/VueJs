9ad94f329e22d9ac5091a0fb8e26e539b207a9eb
Leonardo Santos <leonardojosantos@gmail.com>
2022-08-09 21:23:02 -0300

    Múltiplos eventos
    Neste vídeo você vai aprender sobre a criação de múltiplos eventos com #Vue, uma forma de ativar dois ou mais métodos com apenas uma ação do usuário.

--- projeto_vue/src/components/PrimeiroComponente.vue
+++ projeto_vue/src/components/PrimeiroComponente.vue
@@ -2,15 +2,25 @@
 <template>
     <div>
         <h1>Olá, Vue</h1>
+        
         <p>
             Meu nome é {{ nome }} e sou {{ profissao }}!
         </p>
+
+        <MultiplosEventos />
     </div>
 </template>
 
 <script>
+    import MultiplosEventos from './MultiplosEventos.vue'
+
     export default {
         name: 'PrimeiroComponente',
+        
+        components: {
+            MultiplosEventos
+        },
+
         data() {
             return {
                 nome: "Leonardo",
