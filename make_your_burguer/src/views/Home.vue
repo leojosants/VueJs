ce89ced9dfbeac873fae1e6ecdfd4c1e378c99cb
Leonardo Santos <leonardojosantos@gmail.com>
2022-08-10 08:35:06 -0300

    Criando formulário de cadastro
    Neste vídeo vamos criar um formulário de cadastro em nosso app com #Vue, ele vai servir para o usuário realizar o seu pedido no sistema, posteriormente vamos exibir estes pedidos em uma dashboard.

--- make_your_burguer/src/views/Home.vue
+++ make_your_burguer/src/views/Home.vue
@@ -4,19 +4,21 @@
 
   <div class="main-container">
     <h1>Monte seu burger:</h1>
-    <p>formulário</p>
+    <BurgerForm />
   </div>
 </template>
 
 <script>
   import Banner from '../components/Banner.vue'
+  import BurgerForm from '@/components/BurgerForm.vue';
 
   export default {
     name: 'Home',
 
     components: {
-      Banner
-    }
+    Banner,
+    BurgerForm,
+}
   }
 </script>
 
