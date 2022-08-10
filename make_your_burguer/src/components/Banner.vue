e6f054b802967e6d4d084e21b63597176c145401
Leonardo Santos <leonardojosantos@gmail.com>
2022-08-10 01:14:47 -0300

    Criando banner da aplicação
    Neste vídeo vamos criar o banner principal da nossa aplicação com #Vue, é uma parte estática mas de suma importância, nem sempre estamos lidando com dados dinâmicos, as vezes temos componentes apenas para atender ao layout do app, como este nosso banner.

---
+++ make_your_burguer/src/components/Banner.vue
@@ -0,0 +1,32 @@
+<!--  -->
+<template>
+    <div id="main-banner">
+        <h1>Make Your Burger</h1>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'Banner'
+  }
+</script>
+
+<style scoped>
+    #main-banner {
+      background-image: url('/REPOSITORIO_GITHUB_DESKTOP/VueJs/make_your_burguer/public/img/burger.jpg');
+      background-position: 0 -250px;
+      background-size: cover;
+      height: 500px;
+      display: flex;
+      align-items: center;
+      justify-content: flex-start;
+    }
+
+    #main-banner h1 {
+      color: #fff;
+      text-align: center;
+      font-size: 60px;
+      background-color: #222;
+      padding: 50px 40px;
+    }
+</style>
