ef17fbf1e787c43ea6be1a5b260519241d367e79
Leonardo Santos <leonardojosantos@gmail.com>
2022-08-09 22:35:07 -0300

    Emit
    Neste vídeo você vai aprender em #Vue, como fazer a comunicação de um componente filho para o pai, podendo executar métodos e também realizar outras ações, vamos utilizar o $emit

--- projeto_vue/src/components/Picture.vue
+++ projeto_vue/src/components/Picture.vue
@@ -1,23 +1,29 @@
 <!--  -->
 <template>
     <img :src="avatar" :alt="descricao">
-    <Reutilizacao />
+    <MudarImagem @mudar-imagem="trocarImagem" />
 </template>
 
 <script>
-    import Reutilizacao from './Reutilizacao.vue'
+    import MudarImagem from './MudarImagem.vue'
 
     export default {
         name: 'Picture',
 
         components: {
-            Reutilizacao
+            MudarImagem
         },
 
         data() {
             return {
                 avatar: './img/avatar.png',
                 descricao: 'Leonardo Santos'
+            }
+        },
+
+        methods: {
+            trocarImagem() {
+                this.avatar = '/img/avatar2.png'
             }
         }
     }
